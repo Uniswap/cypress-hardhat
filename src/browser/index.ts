9838f6ac426d0d90c200a76a743ff4b712f01f9e
Zach Pomerantz <zzmp@uniswap.org>
2023-04-26 11:43:49 -0700

    fix: cy typings

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -3,12 +3,23 @@
 import { Network } from '../types/Network'
 import { Utils } from './utils'
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> {
+      hardhat(): Chainable<Utils>
+      task(event: 'hardhat'): Chainable<Network>
+    }
+  }
+}
+
 let hardhat: Utils
 
 cy.hardhat = () => {
   if (hardhat) return cy.wrap(hardhat)
 
-  return cy.task('hardhat').then((env: Network) => {
+  return cy.task('hardhat').then((env) => {
     return (hardhat = new Utils(env))
   })
 }
