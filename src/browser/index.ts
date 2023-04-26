74c9ff9a7784b4a28d7574c9b62f7fd8b024c73b
Zach Pomerantz <zzmp@uniswap.org>
2023-04-26 13:27:10 -0700

    feat: add eip1193

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unused-modules */
 
 import { Network } from '../types/Network'
+import { Eip1193 } from './eip1193'
 import { Utils } from './utils'
 
 declare global {
@@ -9,17 +10,27 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable<Subject> {
       hardhat(): Chainable<Utils>
+      provider(): Chainable<Eip1193>
       task(event: 'hardhat'): Chainable<Network>
     }
   }
 }
 
 let hardhat: Utils
+let provider: Eip1193
 
 cy.hardhat = () => {
   if (hardhat) return cy.wrap(hardhat)
 
   return cy.task('hardhat').then((env) => {
     return (hardhat = new Utils(env))
+  })
+}
+
+cy.provider = () => {
+  if (provider) return cy.wrap(provider)
+
+  return cy.hardhat().then((hardhat) => {
+    return (provider = new Eip1193(hardhat))
   })
 }
