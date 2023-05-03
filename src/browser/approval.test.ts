181b419155759a8c73f524ae5bc220acec108256
cartcrom <39385577+cartcrom@users.noreply.github.com>
2023-05-03 16:48:52 -0400

    refactor: replace number with BigNumber in approval utils (#16)
    * updates `setTokenAllowance` to accept `BigNumberish` as input instead
    of `number`
    * updates `setTokenAllowance` to use `MaxUint256` as default approval
    amount
    * updates `setPermit2Allowance` to use `MaxUint160` as default approval
    amount & 30 days as default expiration time
    * replaces usage of `BigNumber.from(0)` with `0`
    * updates tests to match refactor

--- src/browser/approval.test.ts
+++ src/browser/approval.test.ts
@@ -3,8 +3,9 @@
  * This is expected, and necessary in order collect coverage.
  */
 
+import { MaxUint160, MaxUint256 } from '@uniswap/permit2-sdk'
 import { Token } from '@uniswap/sdk-core'
-import { BigNumber } from 'ethers/lib/ethers'
+import { constants } from 'ethers/lib/ethers'
 
 import setup from '../plugin/setup'
 import { ApprovalUtils } from './approval'
@@ -37,58 +38,95 @@ describe('Approval', () => {
   describe('setTokenAllowance', () => {
     it('approves USDT', async () => {
       const originalAllowance = await approval.getTokenAllowance({ owner, token, spender })
-      expect(originalAllowance.toNumber()).toBe(0)
+      expect(originalAllowance.eq(0)).toBeTruthy()
 
       await approval.setTokenAllowance({ owner, token, spender }, 5)
 
       const updatedAllowance = await approval.getTokenAllowance({ owner, token, spender })
-      expect(updatedAllowance.toNumber()).toBe(5)
+      expect(updatedAllowance.eq(5)).toBeTruthy()
+    })
+    it('approves max token allowance by default', async () => {
+      await approval.setTokenAllowance({ owner, token, spender })
+
+      const updatedAllowance = await approval.getTokenAllowance({ owner, token, spender })
+      expect(updatedAllowance).toMatchObject(constants.MaxUint256)
     })
     it('revokes USDT', async () => {
       await approval.setTokenAllowance({ owner, token, spender }, 5)
       await approval.revokeTokenAllowance({ owner, token, spender })
 
       const allowance = await approval.getTokenAllowance({ owner, token, spender })
-      expect(allowance.toNumber()).toBe(0)
+      expect(allowance.eq(0)).toBeTruthy()
     })
   })
   describe('setTokenAllowanceForPermit2', () => {
     it('approves USDT for Permit2', async () => {
       const originalAllowance = await approval.getTokenAllowanceForPermit2({ owner, token })
-      expect(originalAllowance.toNumber()).toBe(0)
+      expect(originalAllowance.eq(0)).toBeTruthy()
 
       await approval.setTokenAllowanceForPermit2({ owner, token }, 5)
 
       const updatedAllowance = await approval.getTokenAllowanceForPermit2({ owner, token })
-      expect(updatedAllowance.toNumber()).toBe(5)
+      expect(updatedAllowance.eq(5)).toBeTruthy()
+    })
+    it('approves max token allowance by default', async () => {
+      await approval.setTokenAllowanceForPermit2({ owner, token })
+
+      const allowance = await approval.getTokenAllowanceForPermit2({ owner, token })
+      expect(allowance).toMatchObject(MaxUint256)
     })
     it('revokes USDT for Permit2', async () => {
       await approval.setTokenAllowanceForPermit2({ owner, token }, 5)
       await approval.revokeTokenAllowanceForPermit2({ owner, token })
 
       const allowance = await approval.getTokenAllowanceForPermit2({ owner, token })
-      expect(allowance.toNumber()).toBe(0)
+      expect(allowance.eq(0)).toBeTruthy()
     })
   })
   describe('setPermit2Allowance', () => {
     it('permits Universal Router for USDT', async () => {
-      const originalPermit = await approval.getPermit2Allowance({ owner, token })
-      expect(originalPermit.amount.toNumber()).toBe(0)
-      expect(originalPermit.expiration).toBe(0)
+      const originalAllowance = await approval.getPermit2Allowance({ owner, token })
+      expect(originalAllowance.amount.eq(0)).toBeTruthy()
 
-      await approval.setPermit2Allowance({ owner, token }, { amount: BigNumber.from(5), expiration: 1000 })
+      await approval.setPermit2Allowance({ owner, token }, { amount: 5, expiration: 1000 })
 
       const updatedAllowance = await approval.getPermit2Allowance({ owner, token })
-      expect(updatedAllowance.amount.toNumber()).toBe(5)
+      expect(updatedAllowance.amount.eq(5)).toBeTruthy()
       expect(updatedAllowance.expiration).toBe(1000)
     })
+    it('permits max permit allowance by default', async () => {
+      await approval.setPermit2Allowance({ owner, token }, { expiration: 1000 })
+
+      const updatedAllowance = await approval.getPermit2Allowance({ owner, token })
+      expect(updatedAllowance.amount).toMatchObject(MaxUint160)
+    })
+    it('permits with a 30 day expiration by default', async () => {
+      await approval.setPermit2Allowance({ owner, token }, { amount: 5 })
+
+      const updatedAllowance = await approval.getPermit2Allowance({ owner, token })
+      expect(updatedAllowance.expiration).toBeLessThanOrEqual(Math.floor(Date.now() / 1000) + 2_592_000)
+    })
+    it('permits default amount/expiration when no approval is passed', async () => {
+      await approval.setPermit2Allowance({ owner, token })
+
+      const updatedAllowance = await approval.getPermit2Allowance({ owner, token })
+      expect(updatedAllowance.expiration).toBeLessThanOrEqual(Math.floor(Date.now() / 1000) + 2_592_000)
+      expect(updatedAllowance.amount).toMatchObject(MaxUint160)
+    })
+    it('permits default amount/expiration when empty object is passed', async () => {
+      await approval.setPermit2Allowance({ owner, token }, {})
+
+      const updatedAllowance = await approval.getPermit2Allowance({ owner, token })
+      expect(updatedAllowance.expiration).toBeLessThanOrEqual(Math.floor(Date.now() / 1000) + 2_592_000)
+      expect(updatedAllowance.amount).toMatchObject(MaxUint160)
+    })
     it("revokes Universal Router's permit for USDT", async () => {
-      await approval.setPermit2Allowance({ owner, token }, { amount: BigNumber.from(5), expiration: 1000 })
+      await approval.setPermit2Allowance({ owner, token }, { amount: 5, expiration: 1000 })
       await approval.revokePermit2Allowance({ owner, token })
 
       const allowance = await approval.getPermit2Allowance({ owner, token })
 
-      expect(allowance.amount.toNumber()).toBe(0)
+      expect(allowance.amount.eq(0)).toBeTruthy()
       expect(allowance.expiration).toBeLessThan(Date.now() / 1000)
     })
   })
--- src/browser/approval.ts
+++ src/browser/approval.ts
@@ -1,16 +1,18 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
-import { PERMIT2_ADDRESS } from '@uniswap/permit2-sdk'
+import { MaxUint160, MaxUint256, PERMIT2_ADDRESS } from '@uniswap/permit2-sdk'
 import { UNIVERSAL_ROUTER_ADDRESS } from '@uniswap/universal-router-sdk'
-import { BigNumber } from 'ethers/lib/ethers'
+import { BigNumber, BigNumberish } from 'ethers/lib/ethers'
 
 import { AllowanceTransfer__factory, Erc20__factory, Permit2__factory } from '../types'
 import { ImpersonatedSigner } from './signer'
 import { AddressLike } from './types'
 
+function get30DayExpiration(): number {
+  return Math.floor(Date.now() / 1000) + 2_592_000
+}
+
 type ApprovalAddresses = { owner: AddressLike; token: AddressLike; spender: AddressLike }
 type Permit2ApprovalAddresses = { owner: AddressLike; token: AddressLike; spender?: AddressLike }
-
-type Permit2Allowance = { amount: BigNumber; expiration: number }
 
 function normalizeAddressLike(address: AddressLike): string {
   return typeof address === 'string' ? address : address.address
@@ -40,8 +42,12 @@ export class ApprovalUtils {
     return await erc20.allowance(owner, spender)
   }
 
-  /** Sets the amount the spender is allowed by the owner to spend for the token. */
-  async setTokenAllowance(addresses: ApprovalAddresses, amount: number): Promise<void> {
+  /**
+   * Sets the amount the spender is allowed by the owner to spend for the token.
+   *
+   * @param amount - number to set the allowance to (defaults to a max token approval of MaxUint256)
+   * */
+  async setTokenAllowance(addresses: ApprovalAddresses, amount: BigNumberish = MaxUint256): Promise<void> {
     const { owner, token, spender } = normalizeApprovalAddresses(addresses)
 
     const erc20 = Erc20__factory.connect(token, new ImpersonatedSigner(owner, this.provider))
@@ -58,8 +64,12 @@ export class ApprovalUtils {
     return await this.getTokenAllowance({ ...addresses, spender: PERMIT2_ADDRESS })
   }
 
-  /** Sets the amount Permit2 is allowed by the owner to spend for the token. */
-  async setTokenAllowanceForPermit2(addresses: Omit<ApprovalAddresses, 'spender'>, amount: number) {
+  /**
+   * Sets the amount Permit2 is allowed by the owner to spend for the token.
+   *
+   * @param amount - number to set the allowance to (defaults to a max token approval of MaxUint256)
+   * */
+  async setTokenAllowanceForPermit2(addresses: Omit<ApprovalAddresses, 'spender'>, amount: BigNumberish = MaxUint256) {
     return this.setTokenAllowance({ ...addresses, spender: PERMIT2_ADDRESS }, amount)
   }
 
@@ -73,17 +83,22 @@ export class ApprovalUtils {
     owner,
     token,
     spender = this.universalRouterAddress,
-  }: Permit2ApprovalAddresses): Promise<Permit2Allowance> {
+  }: Permit2ApprovalAddresses): Promise<{ amount: BigNumber; expiration: number }> {
     const addresses = normalizeApprovalAddresses({ owner, token, spender })
 
     const permit2 = Permit2__factory.connect(PERMIT2_ADDRESS, this.provider)
     return permit2.allowance(addresses.owner, addresses.token, addresses.spender)
   }
 
-  /** Sets a spender's Permit2 allowance by the owner for the token. Spender is Universal Router by default. */
+  /**
+   * Sets a spender's Permit2 allowance by the owner for the token.
+   *
+   * @param addresses - object containing the `owner`, `token`, and `spender` (`spender` defaults to the Universal Router)
+   * @param allowance - object containing the `amount` (defaults to a max permit approval of MaxUint160) and `expiration` (defaults to 30 days from the current time)
+   * */
   async setPermit2Allowance(
     { owner, token, spender = this.universalRouterAddress }: Permit2ApprovalAddresses,
-    { amount, expiration }: Permit2Allowance
+    { amount = MaxUint160, expiration = get30DayExpiration() } = {} as { amount?: BigNumberish; expiration?: number }
   ): Promise<void> {
     const addresses = normalizeApprovalAddresses({ owner, token, spender })
 
@@ -97,6 +112,6 @@ export class ApprovalUtils {
 
   /** Sets a spender's Permit2 allowance by the owner for the token to 0. Spender is Universal Router by default. */
   async revokePermit2Allowance(addresses: Permit2ApprovalAddresses): Promise<void> {
-    return this.setPermit2Allowance(addresses, { amount: BigNumber.from(0), expiration: 0 })
+    return this.setPermit2Allowance(addresses, { amount: 0, expiration: 0 })
   }
 }
