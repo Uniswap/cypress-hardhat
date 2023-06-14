50855c7a2a3ac1f03e581c7d532be115514d3fea
Zach Pomerantz <zzmp@uniswap.org>
2023-06-14 14:54:46 -0500

    better tests

--- src/browser/provider.test.ts
+++ src/browser/provider.test.ts
@@ -9,31 +9,42 @@ jest.mock('@ethersproject/web', () => ({
 describe('HardhatProvider', () => {
   it('caches network across eth_chainId calls', async () => {
     const provider = new HardhatProvider('http://localhost:8545')
-
     jest.mocked(fetchJson).mockResolvedValueOnce('0x1')
     const network = await provider.getNetwork()
+
     await expect(provider.getNetwork()).resolves.toBe(network)
   })
 
   describe('reset', () => {
     it('allows an earlier blockNumber', async () => {
       const provider = new HardhatProvider('http://localhost:8545')
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x1')
+      await provider.getNetwork()
 
-      jest.mocked(fetchJson).mockReset().mockResolvedValueOnce('0x3').mockResolvedValue('0x2')
-      await expect(provider.send('eth_blockNumber', [])).resolves.toEqual('0x3')
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x3')
+      await expect(provider.getBlockNumber()).resolves.toEqual(3)
+
+      // Without reset, an earlier blockNumber is not accepted.
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x2')
+      await new Promise((resolve) => setTimeout(resolve))
+      await expect(provider.getBlockNumber()).resolves.toEqual(3)
 
       provider.reset()
-      await expect(provider.send('eth_blockNumber', [])).resolves.toEqual('0x2')
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x1')
+      await provider.getNetwork()
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x2')
+      await expect(provider.getBlockNumber()).resolves.toEqual(2)
     })
 
     it('allows network changes', async () => {
       const provider = new HardhatProvider('http://localhost:8545')
 
-      jest.mocked(fetchJson).mockResolvedValueOnce('0x1').mockResolvedValueOnce('0x2')
-      await expect(provider.send('eth_chainId', [])).resolves.toEqual('0x1')
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x1')
+      await expect(provider.getNetwork()).resolves.toEqual(expect.objectContaining({ chainId: 1 }))
 
+      jest.mocked(fetchJson).mockResolvedValueOnce('0x2')
       provider.reset()
-      await expect(provider.send('eth_chainId', [])).resolves.toEqual('0x2')
+      await expect(provider.getNetwork()).resolves.toEqual(expect.objectContaining({ chainId: 2 }))
     })
   })
 })
